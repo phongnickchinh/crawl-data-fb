6b23a60ab5559089270b718ca9da58926fadb46a
DESKTOP-V5ATM3U\dungt <phamphong300111@gmail.com>
2024-08-30 15:12:26 +0700

    feat: Add get_timeline_page function to crawl.py

--- crawl.py
+++ crawl.py
@@ -107,7 +107,87 @@ def get_post_newfeed(driver):
     except:
         print("Fucntion get_post_newfeed error")
 
+# get_timeline of a page
+def get_timeline_page(driver, pagename):
+    try:
+        page_link = "https://mbasic.facebook.com/" + pagename + "?v=timeline"
+        driver.get(page_link)
+        time.sleep(2)
+        count = 0
+        lastest_post_link = "fsdfsdfdsfdsfsd"
+        while True:
+            try:
+                soup = BeautifulSoup(driver.page_source, "html.parser")
 
-get_post_newfeed(driver)
-input("Press Enter to continue...")
+                feed = soup.find("div", id="structured_composer_async_container")
+                section = feed.find("section")
+                list_posts = section.find_all("article", recursive=False)
+
+                with open("timeLine_" + pagename + ".txt", "a", encoding="utf-8") as f:
+                    for post in list_posts:
+                        try:
+
+                            #find a tag have text include "Comment" (example "32 Comment", "Comment", "Comment 1")
+                            a_tag = post.find("a", text=lambda text: text and "omment" in text)
+                            #a link hae alot unnecessary characters, we only need a part of it, cut string when meet the second "&"
+                            link =a_tag['href'] if a_tag is not None else ""
+                            post_link = "https://mbasic.facebook.com" + link[:link.find("&", link.find("&")+1)]
+                            if post_link == lastest_post_link:
+                                break
+                            if count == 0:
+                                lastest_post_link = post_link
+                            print(post_link)
+                            # find div with tag header, this div contains poster's name
+                            poster_div = post.find("header")
+                            poster = poster_div.get_text(strip=True) if poster_div is not None else "Unknown"
+                            # find text content of post, this div is next sibling of div with poster's name
+                            text = poster_div.find_next_sibling("div").get_text(strip=True) if poster_div is not None else ""
+                            count += 1
+                            try:
+                                #find all images in post
+                                list_img = poster_div.find_next_sibling("div").find_next_sibling("div")
+                                list_img = list_img.find_all('a')
+                                picture = ""
+                                for img in list_img:
+                                    picture += "https://mbasic.facebook.com" + img['href'][:img['href'].find("&", img['href'].find("&")+1)] + "\n"
+                            except:
+                                picture = ""
+                            f.write(str(count) + "\n")
+                            f.write(post_link + "\n" + poster + "\n" + text + "\n" + picture + "\n")
+                        except Exception as e:
+                            print("Error when extracting posts")
+                            print(e)
+                            continue
+                
+                #click see more posts is a element which í child of div with id = m_show_more_pager
+                try:
+                    see_more_div = section.find_next_sibling("div")
+                    see_more = see_more_div.find("a")
+                    driver.get("https://mbasic.facebook.com" + see_more['href'])
+                except:
+                    print("Error when loading more posts")
+                    break
+            
+            except Exception as e:
+                print("Error in loop")
+                print(e)
+                break
+            
+    except Exception as e:
+        print("Fucntion get_timeline_page error")
+        print(e)
+    finally:
+        print("Stop at post: ", count)
+
+
+# get_post_newfeed(driver)
+
+# from load_img import export_link
+# export_link("posts.txt", driver)
+# # print("Done")
+pageName  ="BoxGirlVn"
+get_timeline_page(driver, pageName)
+export_link("timeLine_"+ pageName + ".txt", "image_" + pageName, "video_" + pageName, driver)
+
+input("Press Enter to Exit....")
 driver.quit()
