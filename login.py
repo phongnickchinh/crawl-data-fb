4cfaf6314720ce0612014313f89db4fa046ae9ee
DESKTOP-V5ATM3U\dungt <phamphong300111@gmail.com>
2024-09-06 16:10:01 +0700

    add login_w_keys function

--- login.py
+++ login.py
@@ -1,3 +1,6 @@
+from selenium.webdriver.common.by import By
+import time
+
 def login(inputdriver, url = "https://mbasic.facebook.com"):
      # Chuỗi cookie dài
     with open('cookie.txt', 'r') as f:
@@ -19,6 +22,28 @@ def login(inputdriver, url = "https://mbasic.facebook.com"):
     # Thêm từng cookie vào trình duyệt
     for cookie in cookies:
         driver.add_cookie(cookie)
-
+    # Tải lại trang
+    driver.get(url)
     return driver
 
+def login_w_keys(inputdriver, username, password, url_login = "https://www.instagram.com/"):
+    driver = inputdriver
+    driver.get(url_login)
+    time.sleep(5)
+    #find the username input
+    xpath = "//input[@name='username']"
+    username_input = driver.find_element(By.XPATH, xpath)
+    #send the username
+    username_input.send_keys(username)
+    #find the password input
+    xpath = "//input[@name='password']"
+    password_input = driver.find_element(By.XPATH, xpath)
+    #send the password
+    password_input.send_keys(password)
+    #find the login button
+    xpath = "//button[@type='submit']"
+    login_button = driver.find_element(By.XPATH, xpath)
+    #click the login button
+    login_button.click()
+    time.sleep(5)
+    return driver
